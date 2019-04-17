5484f8dafe8234a0b3b35a258d406d6b0fcce4ef
Benjamin Ritchie <benaritchie18@gmail.com>
2019-04-17 04:30:07 -0700

    Local Fonts Added/Font face observer

--- js/application.js
+++ js/application.js
@@ -2,4 +2,13 @@
 layout: null
 ---
 
-{% include_relative _style-picker.js %}
+var fontA = new FontFaceObserver('Lato');
+var fontB = new FontFaceObserver('Lobster');
+
+fontA.load().then(function () {
+  console.log('Lato is available');
+});
+
+fontB.load().then(function () {
+  console.log('Lobster is available');
+});
